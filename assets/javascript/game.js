dd20ebd5f365d1f942c03de91eeb11859cea828c
goldfly <goldfly1@gmail.com>
2020-12-30 03:45:32 -0500

    cosmetic changes I know I overdid by reloading at body tag.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,14 +12,14 @@
             }
   }
   function wrongType() {
-    document.querySelector("#billboard").innerHTML = "You must key a letter....";
+    document.querySelector("#billboard").innerHTML = "You must pick a letter....";
 
   }
   function already() {
-    document.querySelector("#billboard").innerHTML = "Already used, go again: ";
+    document.querySelector("#billboard").innerHTML = "Already used, go again....";
   }
   function bad() {
-    document.querySelector("#billboard").innerHTML = "Bad choice. You are one step closer. Try Again.";
+    document.querySelector("#billboard").innerHTML = "Bad choice.  You are one step closer.  Try Again.";
   }
   function goodbye() {
     document.querySelector("#billboard").innerHTML = "Goodbye to you";
@@ -27,7 +27,7 @@
     setTimeout(restart, 8000);
   }
   function afraid() {
-    document.querySelector("#billboard").innerHTML = "Don't let them see you're afraid kid! Pick a letter." ;
+    document.querySelector("#billboard").innerHTML = "Don't let them see you're afraid kid!  Pick a letter..." ;
   }
 
   function goodbyeImg() {
@@ -96,12 +96,68 @@ function winImg() {
             +'<h4 class="card-text" id="guessed">Already guessed</h4>'
           +'</div>'
         +'</div>'
+        +'<table class="bg-white font-weight-bold m-auto" style="cursor:pointer">'
+        +'<tr>'
+          +'<td id="tdA" onclick="content(this)">A</td>'
+          +'<td id="tdB" onclick="content(this)">B</td>'
+        +'</tr>'
+        +'<tr>'
+          +'<td id="tdC" onclick="content(this)">C</td>'
+          +'<td id="tdD" onclick="content(this)">D</td>'
+        +'</tr>'
+        +'<tr>'
+          +'<td id="tdE" onclick="content(this)">E</td>'
+          +'<td id="tdF" onclick="content(this)">F</td>'
+        +'</tr>'
+        +'<tr>'
+          +'<td id="tdG" onclick="content(this)">G</td>'
+          +'<td id="tdH" onclick="content(this)">H</td>'
+        +'</tr>'
+        +'<tr>'
+          +'<td id="tdI" onclick="content(this)">I</td>'
+          +'<td id="tdJ" onclick="content(this)">J</td>'
+        +'</tr>'
+        +'<tr>'
+          +'<td id="tdK" onclick="content(this)">K</td>'
+          +'<td id="tdL" onclick="content(this)">L</td>'
+        +'</tr>'
+        +'<tr>'
+          +'<td id="tdM" onclick="content(this)">M</td>'
+          +'<td id="tdN" onclick="content(this)">N</td>'
+        +'</tr>'
+      +'</table>'
         +'<div id="wordImg" class="card h-85  mx-auto" style="width: 18rem;">'
           +'<img src="assets/images/word.jpg" class="card-img-top" alt="The WORD">'
           +'<div class="card-body">'
             +'<h4 class="card-text" id="word">The WORD</h4>'
           +'</div>'
         +'</div>'
+        +'<table class="bg-white font-weight-bold m-auto" style="cursor:pointer">'
+          +'<tr>'
+            +'<td id="tdO" onclick="content(this)">O</td>'
+            +'<td id="tdP" onclick="content(this)">P</td>'
+          +'</tr>'
+          +'<tr>'
+            +'<td id="tdQ" onclick="content(this)">Q</td>'
+            +'<td id="tdR" onclick="content(this)">R</td>'
+          +'</tr>'
+          +'<tr>'
+            +'<td id="tdS" onclick="content(this)">S</td>'
+            +'<td id="tdT" onclick="content(this)">T</td>'
+          +'</tr>'
+          +'<tr>'
+            +'<td id="tdU" onclick="content(this)">U</td>'
+            +'<td id="tdV" onclick="content(this)">V</td>'
+          +'</tr>'
+          +'<tr>'
+            +'<td id="tdW" onclick="content(this)">W</td>'
+            +'<td id="tdX" onclick="content(this)">X</td>'
+          +'</tr>'
+          +'<tr>'
+            +'<td id="tdY" onclick="content(this)">Y</td>'
+            +'<td id="tdZ" onclick="content(this)">Z</td>'
+          +'</tr>'
+        +'</table>'
         +'<div class="card h-85 mx-auto" style="width: 18rem;">'
           +'<img src="assets/images/steps.jpg" class="card-img-top" alt="Missed">'
           +'<div class="card-body">'
@@ -109,7 +165,7 @@ function winImg() {
           +'</div>'
         +'</div>'
         +'<div class="col-md-12  text-center  bg-white">'
-          +'<h3 class="" id="billboard">'+"Keep telling them you're innocent! Pick a letter...</h3>"
+          +'<h3 class="" id="billboard">'+"Keep telling them you're innocent!  Pick a letter...</h3>"
         +'</div>'
         +'<p class="col-md-12 text-center bg-white "> &copyMMXX Gonkulishus</p>'
       +'</div>'
@@ -132,6 +188,7 @@ function winImg() {
   }
 
   function logic(){
+    document.querySelector("#td"+selected.toUpperCase()).outerHTML = '<td class="bg-danger">'+selected.toUpperCase()+'</td>'
     if (selected >= "a" && selected <= "z"){
       if (guessed.indexOf(selected) === -1){
       guessed.push(selected);
